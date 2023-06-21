5408cca812004690fb4bf377a3e30341e127b86c
iTz Parsa <111851116+iTz-Parsa@users.noreply.github.com>
2023-06-21 23:33:49 +0330

    Add files via upload

---
+++ v2checker.py
@@ -0,0 +1,32 @@
+# v2 Cheaker | iTzParsa
+
+#!/usr/bin/python3
+from requests import get
+from base64 import b64decode
+from json import loads
+import socket, sys
+
+def get_servers():
+	global servers
+	req = get('https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/sub/splitted/vmess.txt')
+	for s in req.text.splitlines():
+		servers.append(s)
+
+def check_servers():
+	for s in servers:
+		js = loads(b64decode(s.replace("vmess://", "")).decode())
+		add = js['host'] if js['host'] else js['add']
+		port = js['port']
+		so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try: so.connect((add, port))
+		except KeyboardInterrupt: sys.exit(1)
+		except: pass
+		else: print(s)
+
+if __name__ == '__main__':
+	servers = []
+	get_servers()
+	check_servers()
+
+
+d
